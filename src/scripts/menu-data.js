c19fe8e316a402059ef8e43eed91bc0eff95d571
Vineeth Ravoori <145092290+persistent-hustler@users.noreply.github.com>
2024-08-31 11:20:00 +0530

    Complete the Menu Page

---
+++ src/scripts/menu-data.js
@@ -0,0 +1,82 @@
+export const categories = [
+    {
+        heading: 'Primi piatti - Vorspeise',
+        items: [
+            {
+                name: 'Datterini fresci',
+                ingredients: 'Datteltomaten, Burrata, Basilikum',
+                price: 21,
+            },
+            {
+                name: 'Insalata della Casa',
+                ingredients: 'Haussalat, Gartenkräuter, Haussdresing',
+                price: 14,
+            },
+            {
+                name: 'Terrine di Polipo',
+                ingredients: 'Pulpoterrine, Amalfi Zitrone, Sauerrahm, Kerbel',
+                price: 33,
+            },
+        ],
+    },
+    {
+        heading: 'Terra & Mare & More',
+        items: [
+            {
+                name: 'Datterini fresci',
+                ingredients: 'Datteltomaten, Burrata, Basilikum',
+                price: 34,
+            },
+            {
+                name: 'Duetto di Vitello',
+                ingredients: 'Kalbsbäggli & Filet, Barolo - Juise, Kartoffelgratin, Haselnüsse, Trüffelschaum, Karotten',
+                price: 72,
+            },
+            {
+                name: 'Filetto di Spigola',
+                ingredients: 'Wolfsbarsch, Caponata, Balsamico - Gel',
+                price: 64,
+            },
+            {
+                name: 'Involtini Primavera',
+                ingredients: 'Frühlingsrolle, Caponata, Balsamico - Gel',
+                price: 45,
+            },
+            {
+                name: 'Patate Gratinate',
+                ingredients: 'Mille - Feuille, Haselnüsse, Trüffelschaum, Karotten, Sommertrüffel',
+                price: 49,
+            },
+            {
+                name: 'Terrine di Polipo',
+                ingredients: 'Pulpoterrine, Amafli Zitrone, Sauerrahm, Kerbel',
+                price: 48,
+            },
+            {
+                name: 'Terrine di Verdure',
+                ingredients: 'Erfrischende Gemüseterrine, Amafli Zitrone, Sauerrahm, Kerbel',
+                price: 41,
+            },
+        ],
+    },
+    {
+        heading: 'Dessert',
+        items: [
+            {
+                name: 'Crema di Taleggio',
+                ingredients: 'A la Crème brûlee, Früchtebrot',
+                price: 19,
+            },
+            {
+                name: 'Torta Caprese',
+                ingredients: 'Rotwein - Beeren, Vanilleglace, Mandeln',
+                price: 28,
+            },
+            {
+                name: 'Torta di Vinsanto',
+                ingredients: 'Lauwarmes VinSanto Kuchlein, Ricotta, Feigen, Vanilleglace, Schokolade',
+                price: 28,
+            }
+        ]
+    }
+];
--- src/scripts/menu-page.js
+++ src/scripts/menu-page.js
@@ -1,5 +1,33 @@
+import { categories } from "./menu-data";
+
 export const menuPage = () => {
-    const content = document.createElement('ul');
-    content.innerHTML = `<li>Item1 - $pp</li><li>Item2 - $nn<li>`
+    const content = document.createElement('div');
+    categories.forEach((category, index)=>{
+        const categoryDiv = document.createElement('div');
+        const categoryHeader = document.createElement('h2');
+        categoryHeader.innerText = category.heading;
+        categoryHeader.style.paddingBottom = '8px'
+        categoryHeader.style.borderBottom = '1px solid black';
+        categoryDiv.appendChild(categoryHeader);
+
+        category.items.forEach((item)=>{
+            const itemDiv = document.createElement('div');
+            itemDiv.innerHTML = `<h3>${item.name}</h3><p>${item.price}</p>`;
+            itemDiv.setAttribute('style', 'display: flex; justify-content: space-between; align-items: center;');
+            itemDiv.style.marginBottom = '-24px';
+
+            const ingredientDetails = document.createElement('p');
+            ingredientDetails.innerText = item.ingredients;
+            ingredientDetails.style.width = '80%';
+
+            categoryDiv.appendChild(itemDiv);
+            categoryDiv.appendChild(ingredientDetails);
+        });
+
+        categoryDiv.style.marginBottom = '60px';
+
+        content.appendChild(categoryDiv);
+    });
+
     return content;
 }
